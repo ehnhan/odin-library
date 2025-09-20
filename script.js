213da9e472b1ec204454d119ffedfe04314eac66
Eric <e.h.nhan@gmail.com>
2025-09-19 17:42:06 -0700

    Handle adding new books to the page

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayBooks() {
+    books_container.innerHTML = "";
     myLibary.forEach(createBookElement);
 }
 
@@ -34,6 +35,13 @@ function createBookElement(book) {
         }
     }
 
+    let button = document.createElement("button");
+    button.innerText = "-"
+    button.addEventListener("click", () => {
+        // find id and remove from array
+    });
+    div.appendChild(button);
+
     books_container.appendChild(div);
 }
 
@@ -41,3 +49,54 @@ addBookToLibrary("Naruto","Masashi Kishimoto", 330, true);
 addBookToLibrary("One Piece","Eiichiro Oda", 900, true);
 addBookToLibrary("Beserk", "Kentaro Miura", 500, false);
 displayBooks();
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector(".add_book");
+const closeButton = document.querySelector("#cancelBtn");
+const confirmButton = document.querySelector("#confirmBtn");
+
+const book_title = document.querySelector("#new_title");
+const book_author = document.querySelector("#new_author");
+const book_pages = document.querySelector("#new_pages");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+confirmButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+
+    if (!(book_title.value == "" || book_author.value == "" || book_pages.value == "")) {
+        addBookToLibrary(book_title.value, book_author.value, book_pages.value, false);
+        displayBooks();
+        book_title.value = "";
+        book_author.value = "";
+        book_pages.value = "";
+        dialog.close();
+    }
+
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    book_title.value = "";
+    book_author.value = "";
+    book_pages.value = "";
+    dialog.close();
+});
+
+// const modalRoot = document.querySelector("dialog");
+// modalRoot.addEventListener('click', () => {
+//     dialog.close();
+// });
+
+// const modal = document.querySelector("#modal");
+// modal.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     e.stopImmediatePropagation();
+//     return false;
+// });
